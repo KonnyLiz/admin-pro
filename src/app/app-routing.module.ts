b5684018a7601a86eb85496388bcd6316cf1a144
Konny Amaya <konnylizamaya@gmail.com>
2022-06-21 21:47:00 -0600

    #21 Add rutas hijas a pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,29 @@ import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Graphic1Component } from './pages/graphic1/graphic1.component';
 import { Page404Component } from './pages/page404/page404.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'progress', component: ProgressComponent},
-  {path: 'graphic1', component: Graphic1Component},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, 
+  {
+    path: '',
+    component: PagesComponent,
+
+    // arreglo con las rutas hijas del componente
+    // las rutas buscan a partir de la ruta principan y 
+    // busca coincidencia con las hijas
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphic1', component: Graphic1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   // el pathmatch full si esta la ruta vacia lo redirecciona
-  {path: '**', component: Page404Component}, 
+  { path: '**', component: Page404Component },
 ];
 
 @NgModule({
