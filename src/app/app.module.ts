c92f68be2c86c85c98500c3142865cade071db89
Konny Amaya <konnylizamaya@gmail.com>
2022-06-22 21:44:51 -0600

    #33 tarea module para el auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthModule } from './auth/auth.module';
 import { Page404Component } from './pages/page404/page404.component';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     Page404Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
