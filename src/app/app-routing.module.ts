31287bfd06b6a453a6da598e378d47ef89c7c8f3
Konny Amaya <konnylizamaya@gmail.com>
2022-06-23 20:06:18 -0600

    #35 creando rutas hijas para pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graphic1Component } from './pages/graphic1/graphic1.component';
 import { Page404Component } from './page404/page404.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-
-    // arreglo con las rutas hijas del componente
-    // las rutas buscan a partir de la ruta principan y 
-    // busca coincidencia con las hijas
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphic1', component: Graphic1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ]
-  },
-
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // el pathmatch full si esta la ruta vacia lo redirecciona
   { path: '**', component: Page404Component },
 ];
 
+// el pathmatch full si esta la ruta vacia lo redirecciona
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
   // forRoot son las rutas principales
   exports: [RouterModule]
 })
